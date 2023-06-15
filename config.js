0927c64d5b8d4b76717fb0d48ac5e0f9b47f6e3d
Matteo Biasi <matteobiasi@flashbeing.com>
2023-06-15 09:10:40 +0200

    feat: updated phone number

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const DOMAIN = 'ladritscherhof.com';
 export const GOOGLE_MAPS_URL =
   'https://www.google.com/maps/place/Ladritscher+Hof/@46.4219374,11.566205,15z/data=!4m5!3m4!1s0x0:0xe2b6d986fbfa7d95!8m2!3d46.4219374!4d11.566205';
-export const PHONE_NUMBER = '393409265894';
-export const FORMATTED_PHONE_NUMBER = '340 92 65 894';
+export const PHONE_NUMBER = '393402377286';
+export const FORMATTED_PHONE_NUMBER = '340 23 77 286';
